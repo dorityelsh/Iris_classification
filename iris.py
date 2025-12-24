2d7dedb45b30647f8db4f149ec8e4dbdf2a2a290
Dorit Yelshevich <dorityel@gmail.com>
2025-12-24 18:16:53 +0200

    Enhance model evaluation and visualization with cross-validation and Random Forest integration

--- iris.py
+++ iris.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score,cross_val_predict
 import numpy as np
+from sklearn.ensembles import RandomForestClassifier
 
 
 #load the dataset into a pandas DataFrame from sklearn built-in datasets
@@ -54,3 +56,62 @@ manual_model_accuracy = np.mean(manual_predictions == y_train)
 print(f"Manual model accuracy: {manual_model_accuracy*100:.2f}%")
 
 #modling logistic regression
+model =LogisticRegression()
+
+# split training data into training and validation sets to evaluate model performance not on training data
+#xt stands for x train, xv for x validation
+xt, xv, yt, yv = train_test_split(x_train,y_train,test_size=0.25)
+
+model.fit(xt, yt) # Train the model 
+y_pred = model.predict(xv) # Use the trained model to predict labels for new data
+np.mean(y_pred == yv) # same as model.score(xv,yv) Calculate the accuracy by comparing predicted labels to true labels
+
+# model.score(x_train, y_train) - you never want to evalute the model on the same data you trained it on
+print(f"Logistic Regression model accuracy on validation set: {np.mean(y_pred == yv)*100:.2f}%")
+
+#using cross validation to evaluate the model
+"""cross_val_predict generates out-of-sample predictions for each training sample 
+by training the model on k-1 folds and predicting the held-out fold"""
+
+model=LogisticRegression(max_iter=200)
+accouracies = cross_val_score(model, x_train, y_train, cv=5, scoring='accuracy')
+print(np.mean(accouracies))
+y_pred = cross_val_predict(model, x_train, y_train, cv=5)
+predicted_correctly_mask = y_pred == y_train
+x_train[predicted_correctly_mask] # correctly predicted samples
+print(x_train[~predicted_correctly_mask]) # incorrectly predicted samples
+
+# prepare data for plot
+df_predictions = df_train.copy()
+df_predictions["Correct_Prediction"] = predicted_correctly_mask
+df_predictions['prediction'] = y_pred
+df_predictions['prediction_label'] = df_predictions['prediction'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
+sns.scatterplot(data=df_predictions, x='petal length (cm)', y='petal width (cm)', hue='prediction_label', style='Correct_Prediction', palette='deep')
+sns.scatterplot(data=df_predictions, x='petal length (cm)', y='petal width (cm)', hue='target_name', style='Correct_Prediction', palette='deep')
+
+def plot_incorrect_predictions(df_predictions,x_axis_feature, y_axis_feature):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    axs = axs.flatten()
+    sns.scatterplot(data=df_predictions, x=x_axis_feature, y=y_axis_feature, hue='prediction_label', ax =axs[0])
+    sns.scatterplot(data=df_predictions, x=x_axis_feature, y=y_axis_feature, hue='target_name', ax =axs[1]) 
+    sns.scatterplot(data=df_predictions, x=x_axis_feature, y=y_axis_feature, hue='correct_prediction', ax =axs[2])
+    axs[3].set_visible(False)
+    plt.show()
+
+plot_incorrect_predictions(df_predictions,'petal length (cm)', 'petal width (cm)')
+
+# Model Tuning with Random Forest
+# Trying to determine the parameters of your model that maximize performance
+
+
+
+
+
+
+# Try another model - Random Forest
+# model = RandomForestClassifier()
+# accs=cross_val_score(model, x_train, y_train, cv=5, scoring='accuracy')
+# print(f"Random Forest model accuracy on validation set: {np.mean(accs)*100:.2f}%") 
+
+
+  
